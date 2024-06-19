4ab0959653d08359de1f688f0930b3e2f7656d2a
davino1 <s.davino12@studenti.unisa.it>
2024-06-19 12:37:47 +0200

    Update prova.sql
    Abbiamo popolato il database e abbiamo previsto la figura di un admin

--- Database/prova.sql
+++ Database/prova.sql
@@ -12,3 +12,15 @@ CREATE TABLE IF NOT EXISTS utente (
     PRIMARY KEY(email)
   
 );
+INSERT INTO utente (email, nome, cognome, password_hash, ruolo) VALUES
+('mario.rossi@example.com', 'Mario', 'Rossi', 'cane', 0),
+('anna.bianchi@example.com', 'Anna', 'Bianchi', 'pollo', 0),
+('luca.verdi@example.com', 'Luca', 'Verdi', 'ramo', 0),
+('giulia.neri@example.com', 'Giulia', 'Neri', 'gatto', 0),
+('francesco.gialli@example.com', 'Francesco', 'Gialli', 'topo',0 ),
+('paola.marra@example.com', 'Paola', 'Marra', 'leone', 0),
+('davide.azzurri@example.com', 'Davide', 'Azzurri', 'tigre', 0),
+('valentina.rosa@example.com', 'Valentina', 'Rosa', 'orso', 0),
+('giovanni.blu@example.com', 'Giovanni', 'Blu', 'lupo', 0),
+('elisabetta.viola@example.com', 'Elisabetta', 'Viola', 'volpe', 0),
+("RusselCrowel@libero.it","Russel","Crowel","imperoRomano",1);
