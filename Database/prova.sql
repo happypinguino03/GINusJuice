94bee3b23e4c608befcdace6a6de9fbf9eaea6ce
davino1 <s.davino12@studenti.unisa.it>
2024-06-19 17:36:04 +0200

    Update prova.sql
    abbiamo popolato la tabella prodotto

--- Database/prova.sql
+++ Database/prova.sql
@@ -39,3 +39,20 @@ INSERT INTO utente (email, nome, cognome, password_hash, ruolo) VALUES
 ('giovanni.blu@example.com', 'Giovanni', 'Blu', 'lupo', 0),
 ('elisabetta.viola@example.com', 'Elisabetta', 'Viola', 'volpe', 0),
 ("RusselCrowel@libero.it","Russel","Crowel","imperoRomano",1);
+
+INSERT INTO prodotto (nome, descrizione, prezzo, iva, quantità) VALUES
+('Vodka Absolut', 'Ingredienti: Acqua, Alcool, Grano. Allergen: Nessuno.', 20.99, '22', 50),
+('Whiskey Jack Daniels', 'Ingredienti: Alcool, Mais, Segale, Orzo. Allergen: Nessuno.', 35.50, '22', 30),
+('Rum Bacardi', 'Ingredienti: Acqua, Alcool, Melassa. Allergen: Nessuno.', 25.75, '22', 40),
+('Gin Bombay Sapphire', 'Ingredienti: Acqua, Alcool, Bacche di Ginepro. Allergen: Nessuno.', 28.99, '22', 20),
+('Tequila Jose Cuervo', 'Ingredienti: Alcool, Agave. Allergen: Nessuno.', 29.99, '22', 25),
+('Margarita Tropical', 'Ingredienti: Tequila, Triple Sec, Succo di Lime, Sale. Emotions: Freschezza, Energia, Gioia.', 8.50, '22', 100),
+('Cosmopolitan Rosé', 'Ingredienti: Vodka, Triple Sec, Succo di Mirtillo, Succo di Lime. Emotions: Eleganza, Raffinatezza, Semplicità.', 9.00, '22', 100),
+('Mojito di Mezzanotte', 'Ingredienti: Rum, Succo di Lime, Menta, Soda. Emotions: Relax, Vitalità, Freschezza.', 7.50, '22', 120),
+('Negroni Imperiale', 'Ingredienti: Gin, Vermouth Rosso, Campari. Emotions: Intensità, Complessità, Tradizione.', 9.50, '22', 80),
+('Pina Colada Paradiso', 'Ingredienti: Rum, Latte di Cocco, Succo di Ananas. Emotions: Dolcezza, Esotismo, Relax.', 8.00, '22', 90),
+('Bicchiere da Cocktail', 'Utilizzo: Ideale per servire cocktail come Martini, Margarita.', 5.50, '22', 200),
+('Shaker in Acciaio Inox', 'Utilizzo: Per mescolare e raffreddare gli ingredienti dei cocktail.', 15.00, '22', 150),
+('Misurino Jigger', 'Utilizzo: Per misurare con precisione gli ingredienti liquidi dei cocktail.', 7.00, '22', 150),
+('Cucchiaio da Bar', 'Utilizzo: Per mescolare gli ingredienti nel bicchiere e versare con precisione.', 6.00, '22', 180),
+('Filtro Hawthorne', 'Utilizzo: Per trattenere il ghiaccio e i pezzi di frutta mentre si versa il drink.', 8.00, '22', 170);
