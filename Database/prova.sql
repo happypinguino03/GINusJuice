6c8f7ab09ca57e50b877364449aa7f7d36386912
davino1 <s.davino12@studenti.unisa.it>
2024-06-19 12:34:40 +0200

    Update prova.sql
    Abbiamo aggiunto il ruolo per la gestione dell'admin

--- Database/prova.sql
+++ Database/prova.sql
@@ -4,9 +4,11 @@ USE `tsw`;
 DROP TABLE IF EXISTS utente;
 
 CREATE TABLE IF NOT EXISTS utente (
-	email VARCHAR(256) NOT NULL UNIQUE,
+    email VARCHAR(256) NOT NULL UNIQUE,
     nome VARCHAR(50) NOT NULL,
-	cognome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
+    ruolo INT DEFOULT 0,
     PRIMARY KEY(email)
+  
 );
